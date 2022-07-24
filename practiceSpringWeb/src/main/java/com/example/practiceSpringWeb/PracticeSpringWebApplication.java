a1cfd48d2bd2bcb859c0fef4bd721edb665bc12b
Shawn (Eusaku) Clark <106228303+EusakuShawn@users.noreply.github.com>
2022-07-23 19:30:21 -0700

    Add files via upload

---
+++ practiceSpringWeb/src/main/java/com/example/practiceSpringWeb/PracticeSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.practiceSpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpringWebApplication.class, args);
+	}
+
+}
